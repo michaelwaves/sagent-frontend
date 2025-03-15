2578e290e07321e769613aa3c0d4cf3625bebf57
michaelwaves <michaelyu713705@gmail.com>
2025-03-15 15:08:26 -0400

    commit

--- app/Sagent.tsx
+++ app/Sagent.tsx
@@ -1,7 +1,7 @@
 function Sagent() {
     return (
         <iframe
-            src="https://app.near.ai/embed/d0c78238c3e552afe118b28fc052f92ca1b53ad5175720f6229c6308548454d8/sagent/latest"
+            src="https://app.near.ai/embed/d0c78238c3e552afe118b28fc052f92ca1b53ad5175720f6229c6308548454d8/sagent/0.0.1"
             sandbox="allow-scripts allow-popups allow-same-origin allow-forms"
             className="border-none h-[100svh]"
         >
